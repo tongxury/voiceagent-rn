0407e10e1255e71884753a6d18079515ac27b6de
tongxu <tongxurt@gmail.com>
2026-02-03 19:32:24 +0800

    up

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,9 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
+config.resolver.extraNodeModules = {
+    ...config.resolver.extraNodeModules,
+    buffer: require.resolve('buffer'),
+};
+
 module.exports = withNativeWind(config, { input: './src/global.css' });
